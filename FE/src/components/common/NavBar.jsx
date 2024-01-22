fac7e129dfa98ac4f470eb1c81077cacf42a089d
윤주석 <wntjrdbs@gmail.com>
2024-01-22 13:22:09 +0900

    Update FE/src/components/common/NavBar.jsx

--- FE/src/components/common/NavBar.jsx
+++ FE/src/components/common/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import CloseButton from "./closeButton";
+import CloseButton from "./CloseButton";
 import { useSelector } from "react-redux";
 
 function NavBar() {
