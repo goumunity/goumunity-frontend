faf94ec76bd2f75cae11b500f25def10aadbab7c
wkd940709 <wkd940709@naver.com>
2024-01-26 09:48:54 +0900

    refacotr: import Syntax

--- FE/src/components/common/NavBar.jsx
+++ FE/src/components/common/NavBar.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
-import CloseButton from "./closeButton";
+import CloseButton from "./CloseButton";
 import { useSelector } from "react-redux";
 
 function NavBar() {
