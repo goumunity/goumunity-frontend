9088da0cee1c522d455dee42ae825d46cb982d23
전예은 <ye_eun12@naver.com>
2024-02-06 09:18:06 +0900

    :

--- FE/src/components/common/ProfileImage.jsx
+++ FE/src/components/common/ProfileImage.jsx
@@ -1,14 +1,9 @@
 import defaultMaleIcon from '../../assets/svgs/defaultMaleIcon.svg';
 
-
 function ProfileImage({ size, profileImage, ...props }) {
   return (
     <>
-<<<<<<< HEAD
-      {/* <div className={`w-6 h-6 rounded-full  border-2 overflow-hidden cursor-pointer`}>
-=======
       <div className={`rounded-full  border-2 overflow-hidden cursor-pointer`}>
->>>>>>> FE
         {profileImage ? (
           <label htmlFor='profileImg' className=''>
             <img
@@ -26,7 +21,7 @@ function ProfileImage({ size, profileImage, ...props }) {
             />
           </label>
         )}
-        
+
         <input
           type='file'
           accept='image/*'
@@ -34,7 +29,8 @@ function ProfileImage({ size, profileImage, ...props }) {
           {...props}
           className='hidden'
         />
-      </div> */}
+      </div>{' '}
+      */}
       <div className={`rounded-full  border-2 overflow-hidden cursor-pointer`}>
         {profileImage ? (
           <label htmlFor='profileImg' className=''>
