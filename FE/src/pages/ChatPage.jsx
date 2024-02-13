08348d66b2afd44174473d8c6848a561640e352a
김규현 <kgh2120@gmail.com>
2024-02-13 13:27:21 +0900

    fix: 채팅방에 채팅방 제목 그대로 전달하기

--- FE/src/pages/ChatPage.jsx
+++ FE/src/pages/ChatPage.jsx
@@ -8,6 +8,7 @@ import instance from '@/utils/instance.js';
 function ChatPage() {
     const [isSearchMode, setIsSearchMode] = useState(true);
     const [chatRoomId, setChatRoomId] = useState(null);
+    const [chatRoomName, setChatRoomName] = useState('');
     const [messages, setMessages] = useState([]);
     const [hasNext, setHasNext] = useState(true);
     const [myChatRooms, setMyChatRooms] = useState([]);
@@ -16,9 +17,9 @@ function ChatPage() {
     const [searchTime, setSearchTime] = useState(new Date().getTime());
     const observerRef = useRef();
 
-  const handleClickMySection = () => {
-    setIsSearchMode(false);
-  };
+    const handleClickMySection = () => {
+        setIsSearchMode(false);
+    };
 
     const handleSearchMode = () => {
         setIsSearchMode(true)
@@ -28,20 +29,21 @@ function ChatPage() {
     const room = useRef(null);
     const logId = useRef();
     const fetchChatRoomData = async () => {
-        try{
+        try {
             setIsLoading(true)
             const res = await instance.get(`/api/users/my/chat-rooms?page=${pageNum}&size=100&time=${searchTime}`);
             setHasNext(res.data.hasNext);
             setMyChatRooms(prev => [...prev, ...res.data.contents]);
-        }catch (error) {
+        } catch (error) {
             console.error(error)
         }
         setIsLoading(false)
     }
     const connect = () => {
         client.current = new StompJs.Client({
-            brokerURL: 'wss://i10a408.p.ssafy.io/api/goumunity-chat',
-            onConnect : resubscribe
+            // brokerURL: 'wss://i10a408.p.ssafy.io/api/goumunity-chat',
+            brokerURL: 'ws://localhost:8080/goumunity-chat',
+            onConnect: resubscribe
         });
         client.current.activate();
     };
@@ -52,7 +54,7 @@ function ChatPage() {
     };
 
     const logLastAccessTime = async () => {
-        if(!logId.current)
+        if (!logId.current)
             return;
         const res = await instance.patch(`/api/chat-rooms/${logId.current}/disconnect`);
     }
@@ -65,97 +67,101 @@ function ChatPage() {
         }
     }
 
-  const subscribe = (value) => {
-    room.current = client.current.subscribe(`/topic/${value}`, (chat) => {
-      setMessages((prev) => [...prev, JSON.parse(chat.body)]);
-    });
-  };
+    const subscribe = (value) => {
+        room.current = client.current.subscribe(`/topic/${value}`, (chat) => {
+            setMessages((prev) => [...prev, JSON.parse(chat.body)]);
+        });
+    };
 
-  const onMessageSend = (message, chatType) => {
-    client.current.publish({
-      destination: `/ws/messages/${chatRoomId}`,
-      body: JSON.stringify({ content: message, chatType: chatType }),
-    });
-  };
-  const onJoinedRoomClicked = (id) => {
-    if (room.current !== null) room.current.unsubscribe();
-    if (id !== logId.current) logLastAccessTime();
-    setChatRoomId(id);
-    subscribe(id);
-    logId.current = id;
-    setMessages([]);
-  };
-  useEffect(() => {
-    connect();
-    return async () => disconnect();
-  }, []);
+    const onMessageSend = (message, chatType) => {
+        client.current.publish({
+            destination: `/ws/messages/${chatRoomId}`,
+            body: JSON.stringify({content: message, chatType: chatType}),
+        });
+    };
+    const onJoinedRoomClicked = (id) => {
+        if (logId.current === id) return;
+        if (room.current !== null) room.current.unsubscribe();
+        if (id !== logId.current) logLastAccessTime();
 
-  const lastChatRoomRef = useCallback(
-    (node) => {
-      if (isLoading) return;
-      if (observerRef.current) observerRef.current.disconnect();
-      observerRef.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasNext) {
-          setPageNum((prevPageNumber) => prevPageNumber + 1);
-        }
-      });
+        setChatRoomName(myChatRooms.find(room => room.chatRoomId === id).title)
+        setChatRoomId(id);
+        subscribe(id);
+        logId.current = id;
+        setMessages([]);
+    };
+    useEffect(() => {
+        connect();
+        return async () => disconnect();
+    }, []);
 
-      if (node) observerRef.current.observe(node);
-    },
-    [isLoading, hasNext]
-  );
-  useEffect(() => {
-    fetchChatRoomData();
-  }, [pageNum]);
+    const lastChatRoomRef = useCallback(
+        (node) => {
+            if (isLoading) return;
+            if (observerRef.current) observerRef.current.disconnect();
+            observerRef.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasNext) {
+                    setPageNum((prevPageNumber) => prevPageNumber + 1);
+                }
+            });
 
-  //외부 스크롤 막음
-  useEffect(() => {
-    document.body.style.cssText = `
+            if (node) observerRef.current.observe(node);
+        },
+        [isLoading, hasNext]
+    );
+    useEffect(() => {
+        fetchChatRoomData();
+    }, [pageNum]);
+
+    //외부 스크롤 막음
+    useEffect(() => {
+        document.body.style.cssText = `
       position: fixed; 
       top: -${window.scrollY}px;
       overflow-y: scroll;
       width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
-  }, []);
+        return () => {
+            const scrollY = document.body.style.top;
+            document.body.style.cssText = '';
+            window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+        };
+    }, []);
 
-  return (
-    <div className='flex w-full h-full'>
-      <div className='w-1/6 bg-bright'>
-        <ChatMySection
-          refCallback={lastChatRoomRef}
-          myChatRooms={myChatRooms}
-          setMyChatRooms={setMyChatRooms}
-          handleJoinChatRoom={onJoinedRoomClicked}
-          handleClickMySection={handleClickMySection}
-          isLoaded={isSearchMode}
-        />
-      </div>
-      <div className='w-5/6'>
-        <div className=' divide-x divide-entrance'>
-            {isSearchMode ? (
-              <ChatRecommendedSection
-                myChatRooms={myChatRooms}
-                setMyChatRooms={setMyChatRooms}
-              />
-            ) : (
-              <ChatTalkSection
-                  id={chatRoomId}
-                  onMessageSend={onMessageSend}
-                  setMessages={setMessages}
-                  messages={messages}
-                  setIsSearchMode={setIsSearchMode}
-                  setMyChatRooms={setMyChatRooms}
-                  myChatRooms={myChatRooms}
-              />
-            )}
+    return (
+        <div className='flex w-full h-full'>
+            <div className='w-1/6 bg-bright'>
+                <ChatMySection
+                    refCallback={lastChatRoomRef}
+                    myChatRooms={myChatRooms}
+                    setMyChatRooms={setMyChatRooms}
+                    handleJoinChatRoom={onJoinedRoomClicked}
+                    handleClickMySection={handleClickMySection}
+                    isLoaded={isSearchMode}
+                />
+            </div>
+            <div className='w-5/6'>
+                <div className=' divide-x divide-entrance'>
+                    {isSearchMode ? (
+                        <ChatRecommendedSection
+                            myChatRooms={myChatRooms}
+                            setMyChatRooms={setMyChatRooms}
+                        />
+                    ) : (
+                        <ChatTalkSection
+                            id={chatRoomId}
+                            chatRoomTitle={chatRoomName}
+                            onMessageSend={onMessageSend}
+                            setMessages={setMessages}
+                            messages={messages}
+                            setIsSearchMode={setIsSearchMode}
+                            setMyChatRooms={setMyChatRooms}
+                            myChatRooms={myChatRooms}
+                        />
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ChatPage;
