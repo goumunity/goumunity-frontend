319f61050f203afc4081144d4e1e888e67498981
kyoohyun <kgh2120@naver.com>
2024-02-15 22:22:39 +0900

    feat: axios error에서 401이 나오면 세션 스토리지를 날리고, 랜딩페이지로 이동

--- FE/src/utils/instance.js
+++ FE/src/utils/instance.js
@@ -4,5 +4,16 @@ const { VITE_API_DEV } = import.meta.env;
 
 const instance = axios.create({ baseURL: VITE_API_DEV , withCredentials : true})
 // const instance = instance.create({ baseURL: import.meta.env.VITE_API_DEPLOY })
+instance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        if (error.response.status === 401) {
+            sessionStorage.clear();
+            window.location.href='/landing';
+        }
+    }
+)
 
 export default instance
